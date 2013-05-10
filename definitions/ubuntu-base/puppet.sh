aa046915f08f27b39c3bfb610e0b46343dff73f7
Stefano Zanella <zanella.stefano@gmail.com>
2013-05-10 12:59:56 +0200

    Add definition for Ubuntu Server 12.10.
    Not checked very well after install, but seems good.

---
+++ definitions/ubuntu-base/puppet.sh
@@ -0,0 +1,13 @@
+if [ -f .veewee_params ]
+then
+  . .veewee_params
+fi
+
+# Add puppet user and group
+adduser --system --group --home /var/lib/puppet puppet
+
+wget -O puppetlabs-release.deb http://apt.puppetlabs.com/puppetlabs-release-quantal.deb
+dpkg -i puppetlabs-release.deb
+rm puppetlabs-release.deb
+apt-get -y update
+apt-get -y install puppetmaster puppet facter
