af0101166977c27eab78ce6d91db9bf279eacf7d
Stefano Zanella <zanella.stefano@gmail.com>
2013-07-04 19:53:44 +0200

    Fix Virtualbox Guest Additions installation.

--- definitions/ubuntu-base-oss/virtualbox.sh
+++ definitions/ubuntu-base-oss/virtualbox.sh
@@ -5,7 +5,7 @@ fi
 
 # Installing the virtualbox guest additions
 apt-get -y install dkms
-VBOX_VERSION=$(cat /home/ops/.vbox_version)
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
