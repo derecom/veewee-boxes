ea9a1c9ce9181ecaf724d19ee2559556f4279843
Stefano Zanella <zanella.stefano@gmail.com>
2013-07-19 00:08:08 +0200

    Fuck you line wrap.

--- definitions/centos-base-oss/repos.sh
+++ definitions/centos-base-oss/repos.sh
@@ -2,5 +2,4 @@
 
 basearch=`uname -p`
 yum install -y http://ftp.upjs.sk/pub/mirrors/epel/6/${basearch}/epel-release-6-8.noarch.rpm
-yum install -y
-http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.${basearch}.rpm
+yum install -y http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.${basearch}.rpm
