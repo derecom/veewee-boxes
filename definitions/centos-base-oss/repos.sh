c391b8cf33cd39d8c184ac9ae8aac4eef6fb7bd3
Stefano Zanella <zanella.stefano@gmail.com>
2013-07-18 23:39:52 +0200

    Fix centos-base-oss definition.

--- definitions/centos-base-oss/repos.sh
+++ definitions/centos-base-oss/repos.sh
@@ -1,4 +1,6 @@
 # Install EPEL and RPMForge additional repos
 
-yum install -y http://ftp.upjs.sk/pub/mirrors/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum install -y http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+basearch=`uname -p`
+yum install -y http://ftp.upjs.sk/pub/mirrors/epel/6/${basearch}/epel-release-6-8.noarch.rpm
+yum install -y
+http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.${basearch}.rpm
